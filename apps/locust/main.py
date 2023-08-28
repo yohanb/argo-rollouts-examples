d46545ba1f8e9439659ba74b12e58b3b309982d9
Yohan Belval <ybelval@turo.com>
2023-08-28 20:12:53 +0000

    feat: use helmCharts for locust

--- apps/locust/main.py
+++ apps/locust/main.py
@@ -1,12 +1,15 @@
 from locust import HttpUser, task
+import newrelic.agent
+
+# Configure New Relic with your license key
+newrelic.agent.initialize('/home/locust/newrelic.ini')
+
 
 class Color(HttpUser):
     @task
+    @newrelic.agent.background_task()
     def color(self):
-        response = self.client.get("/color")
-
-        # Extract the response data as a string
-        response_data = response.text
-
-        with open("response_data.log", "a") as f:
-            f.write(response_data + "\n")
+        with self.get_session() as session:
+            response = session.get("/color")
+            application = newrelic.agent.application()
+            newrelic.agent.record_custom_event('rollout_get_color', {'color': response.text}, application)
