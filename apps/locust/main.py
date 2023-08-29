25f8eb8f5f37ff211476d6567cc8e98b43c09224
Yohan Belval <ybelval@turo.com>
2023-08-29 16:31:00 +0000

    feat: add kubesealer

--- apps/locust/main.py
+++ apps/locust/main.py
@@ -1,15 +1,15 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 import newrelic.agent
 
 # Configure New Relic with your license key
 newrelic.agent.initialize('/home/locust/newrelic.ini')
 
-
-class Color(HttpUser):
+class Rollout(HttpUser):
+    wait_time = between(0.5, 3)
     @task
     @newrelic.agent.background_task()
     def color(self):
-        with self.get_session() as session:
-            response = session.get("/color")
-            application = newrelic.agent.application()
-            newrelic.agent.record_custom_event('rollout_get_color', {'color': response.text}, application)
+        response = self.client.get(url="/color")
+        application = newrelic.agent.application()
+        newrelic.agent.record_custom_event('rollout_get_color', {'color': response.text}, application)
+        self.client.close()
